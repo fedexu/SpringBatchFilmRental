dcf7ba5da7a9845a1d64a7bc70fb93463891cc70
Ferrarotti <dario.ferrarotti@capgemini.com>
2019-02-22 09:55:38 +0100

    adding scheduled call

--- src/main/java/com/filmrental/scheduled/ScheduledTasks.java
+++ src/main/java/com/filmrental/scheduled/ScheduledTasks.java
@@ -4,6 +4,10 @@ import java.time.format.DateTimeFormatter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +16,18 @@ public class ScheduledTasks {
 	
 	 private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 	 private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-	
-//	 @Scheduled(fixedDelay = 2000) 
-	 public void folderCheck() {
-		 logger.info("SCHEDULATO");
-	 }
+	 @Autowired
+		JobLauncher jobLauncher;
+	 
+	 @Autowired
+		Job importActorJob;
+	 	
+	 @Scheduled(fixedDelay = 2000) 
+	 public String executeJob() throws Exception {
+
+			jobLauncher.run(importActorJob,
+					new JobParametersBuilder().addLong("timestamp", System.currentTimeMillis()).toJobParameters());
+
+			return "launched";
+		}
 }
